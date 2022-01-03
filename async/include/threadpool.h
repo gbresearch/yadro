860855c24d3b7dad5dc2fd57a0a152756c04e7f1
gene <gene@bigstream.co>
2022-01-03 00:48:13 -0800

    refactor

--- async/include/threadpool.h
+++ async/include/threadpool.h
@@ -36,29 +36,11 @@ namespace gbr
             //---------------------------------------------------------------------
             struct callable { virtual ~callable() = default; virtual void call() = 0; };
 
-            using task_t = std::unique_ptr<detail::callable>;
             //---------------------------------------------------------------------
             // function_wrapper: a wrapper class for function object and arguments
             template<class F, class...Args>
-            class function_wrapper final : public callable
+            struct function_wrapper final : public callable
             {
-                F f;
-                std::tuple<Args...> args;
-                using R = decltype(std::invoke(std::forward<F>(std::declval<F>()), std::declval<Args>()...));
-
-                decltype(auto) invoke() { return std::apply(std::forward<F>(f), args); }
-
-                std::promise<R> p;
-
-                template<class T = R>
-                std::enable_if_t<std::is_void_v<T>> 
-                    set_value() { invoke(); p.set_value(); }
-
-                template<class T = R>
-                std::enable_if_t<!std::is_void_v<T>> 
-                    set_value() { p.set_value(invoke()); }
-
-            public:
                 template<class Fun, class ... A>
                 function_wrapper(Fun&& f, A&&... args) : f(std::forward<Fun>(f)), args(std::forward<A>(args)...) {}
 
@@ -71,15 +53,96 @@ namespace gbr
                     try { set_value(); }
                     catch(...) { p.set_exception(std::current_exception()); }
                 }
+            private:
+                F f;
+                std::tuple<Args...> args;
+                using R = decltype(std::invoke(std::forward<F>(std::declval<F>()), std::declval<Args>()...));
+                std::promise<R> p;
+
+                decltype(auto) invoke() { return std::apply(std::forward<F>(f), args); }
+
+                template<class T = R>
+                std::enable_if_t<std::is_void_v<T>>
+                    set_value() { invoke(); p.set_value(); }
+
+                template<class T = R>
+                std::enable_if_t<!std::is_void_v<T>>
+                    set_value() { p.set_value(invoke()); }
             };
         }
 
         //---------------------------------------------------------------------
         // threadpool class: execute pooled tasks (tasks should not use locks)
         template<template<class T> class TaskContainer = task_queue>
-        class threadpool final
+        struct threadpool final
         {
-            TaskContainer<detail::task_t> _tasks;
+            //-----------------------------------------------------------------
+            threadpool(unsigned max_threads, std::function<void()> on_empty)
+                : _max_threads(max_threads), _on_empty(on_empty)
+            {}
+
+            //-----------------------------------------------------------------
+            using sleep_duration_t = decltype(std::chrono::milliseconds(0));
+            threadpool(unsigned max_threads, sleep_duration_t sleep_duration)
+                : threadpool(max_threads, [=] { std::this_thread::sleep_for(sleep_duration); })
+            {}
+
+            //-----------------------------------------------------------------
+            explicit threadpool(unsigned max_threads = std::thread::hardware_concurrency())
+                : threadpool(max_threads, [] { std::this_thread::yield(); })
+            {}
+
+            //-----------------------------------------------------------------
+            explicit threadpool(std::function<void()> on_empty)
+                : threadpool(std::thread::hardware_concurrency(), on_empty)
+            {}
+
+
+            //-----------------------------------------------------------------
+            ~threadpool() { clear(); }
+
+            //-----------------------------------------------------------------
+            void join()
+            {
+                std::lock_guard<std::mutex> _(_m_thread);
+                for(auto&& t : _threads)
+                    t.join();
+            }
+
+            //-----------------------------------------------------------------
+            void detach()
+            {
+                std::lock_guard<std::mutex> _(_m_thread);
+                for(auto&& t : _threads)
+                    t.detach();
+            }
+
+            //-----------------------------------------------------------------
+            void clear()
+            {
+                _finish = true;
+                join();
+                _tasks.clear();
+                _threads.clear();
+            }
+
+            //-----------------------------------------------------------------
+            // enqueue the task and return a future
+            template<class F, class...Args>
+            auto operator()(F&& f, Args&&... args)&
+            {
+                _finish = false;
+                auto t = std::make_unique<detail::function_wrapper<std::decay_t<F>, std::decay_t<Args>...>>(std::forward<F>(f), std::forward<Args>(args)...);
+                auto ftr = t->get_future();
+                std::lock_guard<std::mutex> _(_m_task);
+                _tasks.enqueue(std::move(t));
+                inc_threads();
+                return ftr;
+            }
+
+        private:
+            using task_t = std::unique_ptr<detail::callable>;
+            TaskContainer<task_t> _tasks;
             std::vector<std::thread> _threads;
             std::mutex _m_thread;
             std::mutex _m_task;
@@ -126,74 +189,6 @@ namespace gbr
                 }
             }
 
-        public:
-            //-----------------------------------------------------------------
-            threadpool(unsigned max_threads, std::function<void()> on_empty)
-                : _max_threads(max_threads), _on_empty(on_empty)
-            {
-            }
-
-            //-----------------------------------------------------------------
-            using sleep_duration_t = decltype(std::chrono::milliseconds(0));
-            threadpool(unsigned max_threads, sleep_duration_t sleep_duration)
-                : threadpool(max_threads, [=] { std::this_thread::sleep_for(sleep_duration); })
-            {
-            }
-
-            //-----------------------------------------------------------------
-            explicit threadpool(unsigned max_threads = std::thread::hardware_concurrency())
-                : threadpool(max_threads, [] { std::this_thread::yield(); })
-            {
-            }
-
-            //-----------------------------------------------------------------
-            explicit threadpool(std::function<void()> on_empty)
-                : threadpool(std::thread::hardware_concurrency(), on_empty)
-            {
-            }
-
-
-            //-----------------------------------------------------------------
-            ~threadpool() { clear(); }
-
-            //-----------------------------------------------------------------
-            void join()
-            {
-                std::lock_guard<std::mutex> _(_m_thread);
-                for(auto&& t : _threads)
-                    t.join();
-            }
-
-            //-----------------------------------------------------------------
-            void detach()
-            {
-                std::lock_guard<std::mutex> _(_m_thread);
-                for(auto&& t : _threads)
-                    t.detach();
-            }
-
-            //-----------------------------------------------------------------
-            void clear() 
-            { 
-                _finish = true; 
-                join();
-                _tasks.clear();
-                _threads.clear();
-            }
-
-            //-----------------------------------------------------------------
-            // enqueue the task and return a future
-            template<class F, class...Args>
-            auto operator()(F&& f, Args&&... args) &
-            {
-                _finish = false;
-                auto t = std::make_unique<detail::function_wrapper<std::decay_t<F>, std::decay_t<Args>...>>(std::forward<F>(f), std::forward<Args>(args)...);
-                auto ftr = t->get_future();
-                std::lock_guard<std::mutex> _(_m_task);
-                _tasks.enqueue(std::move(t));
-                inc_threads();
-                return ftr;
-            }
         };
     }
 }
